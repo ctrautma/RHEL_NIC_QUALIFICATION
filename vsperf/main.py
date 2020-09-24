ee30446945aa5f2e0b93a5f1cd441dc2108b9615
HeKai <hewang@redhat.com>
2020-09-24 10:55:32 +0800

    update test adding ovs dpdk pci bus bridge test

--- vsperf/main.py
+++ vsperf/main.py
@@ -512,8 +512,50 @@ def ovs_bridge_with_kernel(nic1_name, nic2_name):
     run(cmd)
     return 0
 
+def ovs_bridge_with_dpdk_with_pci_bus(nic1_bus, nic2_bus, mtu_val, pmd_cpu_mask):
+    cmd = f"""
+	modprobe openvswitch
+	systemctl stop openvswitch
+	sleep 3
+	systemctl start openvswitch
+	sleep 3
+	ovs-vsctl --if-exists del-br ovsbr0
+	sleep 5
 
-def ovs_bridge_with_dpdk(nic1_mac, nic2_mac, mtu_val, pmd_cpu_mask):
+	ovs-vsctl set Open_vSwitch . other_config={{}}
+	ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
+	ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-socket-mem="4096,4096"
+	ovs-vsctl set Open_vSwitch . other_config:pmd-cpu-mask="{pmd_cpu_mask}"
+    ovs-vsctl --no-wait set Open_vSwitch . other_config:vhost-iommu-support=true
+	systemctl restart openvswitch
+	sleep 3
+	ovs-vsctl add-br ovsbr0 -- set bridge ovsbr0 datapath_type=netdev
+
+    ovs-vsctl add-port ovsbr0 dpdk0 -- set Interface dpdk0 type=dpdk options:dpdk-devargs={nic1_bus} mtu_request={mtu_val}
+    ovs-vsctl add-port ovsbr0 dpdk1 -- set Interface dpdk1 type=dpdk options:dpdk-devargs={nic2_bus} mtu_request={mtu_val}
+
+    ovs-vsctl add-port ovsbr0 vhost0 -- set interface vhost0 type=dpdkvhostuserclient options:vhost-server-path=/tmp/vhost0 mtu_request={mtu_val}
+    ovs-vsctl add-port ovsbr0 vhost1 -- set interface vhost1 type=dpdkvhostuserclient options:vhost-server-path=/tmp/vhost1 mtu_request={mtu_val}
+
+    ovs-vsctl set Interface dpdk0  ofport_request=1
+    ovs-vsctl set Interface dpdk1  ofport_request=2
+    ovs-vsctl set Interface vhost0 ofport_request=3
+    ovs-vsctl set Interface vhost1 ofport_request=4
+
+    ovs-ofctl del-flows ovsbr0
+    /usr/bin/ovs-ofctl -O OpenFlow13 --timeout 10 add-flow ovsbr0 idle_timeout=0,in_port=1,action=output:3
+    /usr/bin/ovs-ofctl -O OpenFlow13 --timeout 10 add-flow ovsbr0 idle_timeout=0,in_port=3,action=output:1
+    /usr/bin/ovs-ofctl -O OpenFlow13 --timeout 10 add-flow ovsbr0 idle_timeout=0,in_port=2,action=output:4
+    /usr/bin/ovs-ofctl -O OpenFlow13 --timeout 10 add-flow ovsbr0 idle_timeout=0,in_port=4,action=output:2
+	#ovs-ofctl add-flow ovsbr0 actions=NORMAL
+
+	sleep 2
+	ovs-vsctl show
+    """
+    run(cmd)
+    return 0
+
+def ovs_bridge_with_dpdk_with_mac(nic1_mac, nic2_mac, mtu_val, pmd_cpu_mask):
     cmd = f"""
 	modprobe openvswitch
 	systemctl stop openvswitch
@@ -1124,6 +1166,10 @@ def ovs_dpdk_pvp_test(q_num,mtu_val,pkt_size,cont_time):
     nic2_name = get_env("NIC2")
     nic1_mac = my_tool.get_mac_from_name(nic1_name)
     nic2_mac = my_tool.get_mac_from_name(nic2_name)
+    nic1_businfo = my_tool.get_bus_from_name(nic1_name)
+    nic2_businfo = my_tool.get_bus_from_name(nic2_name)
+    nic_driver = my_tool.get_nic_driver_from_name(nic1_name)
+
     numa_node = bash(f"cat /sys/class/net/{nic1_name}/device/numa_node").value()
 
     log("enable dpdk now")
@@ -1135,11 +1181,17 @@ def ovs_dpdk_pvp_test(q_num,mtu_val,pkt_size,cont_time):
     if q_num == 1:
         cpu_mask = my_tool.get_pmd_masks(" ".join(pmd_cpu_2_list))
         vcpu_list = [get_env("VCPU1"),get_env("VCPU2"),get_env("VCPU3")]
-        ovs_bridge_with_dpdk(nic1_mac,nic2_mac,mtu_val,cpu_mask)
+        if "mlx" in nic_driver:
+            ovs_bridge_with_dpdk_with_mac(nic1_mac,nic2_mac,mtu_val,cpu_mask)
+        else:
+            ovs_bridge_with_dpdk_with_pci_bus(nic1_businfo,nic2_businfo,mtu_val,cpu_mask)
     else:
         cpu_mask = my_tool.get_pmd_masks(" ".join(pmd_cpu_4_list))
         vcpu_list = [get_env("VCPU1"),get_env("VCPU2"),get_env("VCPU3"),get_env("VCPU4"),get_env("VCPU5")]
-        ovs_bridge_with_dpdk(nic1_mac,nic2_mac,mtu_val,cpu_mask)
+        if "mlx" in nic_driver:
+            ovs_bridge_with_dpdk_with_mac(nic1_mac,nic2_mac,mtu_val,cpu_mask)
+        else:
+            ovs_bridge_with_dpdk_with_pci_bus(nic1_businfo,nic2_businfo,mtu_val,cpu_mask)
     
     log("update guest xml config file")
     new_xml = "g1.xml"
