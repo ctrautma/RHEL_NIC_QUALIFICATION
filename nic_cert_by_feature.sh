6b5c03d470f19075c828b3be30f4750272803137
Zhiqiang Fang <zfang@redhat.com>
2021-09-21 15:17:14 -0400

    Update nic_cert_by_feature.sh

--- nic_cert_by_feature.sh
+++ nic_cert_by_feature.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. env.sh
+. test_env.sh
 . ovs-perf-test/ovs_perf_runtest.sh
 
 
@@ -18,6 +18,7 @@ throughput_dut_init()
 		yum -y install $ovs_rpm_path $ovs_selinux_rpm_path $dpdk_rpm_path $dpdk_tools_rpm_path
 		git clone -b feature-cert --single-branch https://github.com/ctrautma/RHEL_NIC_QUALIFICATION.git
 		yum install -y ~/RHEL_NIC_QUALIFICATION/throughput-test/lrzip-0.616-5.el7.x86_64.rpm
+		cp /root/test_env.sh /root/RHEL_NIC_QUALIFICATION/throughput-test/
 		grubby --args='intel_iommu=on iommu=pt default_hugepagesz=1G hugepagesz=1G hugepages=32' --update-kernel=$(grubby --default-kernel)
 		echo "isolated_cores=$PMD_CPU_1,$PMD_CPU_2,$PMD_CPU_3,$PMD_CPU_4,$VCPU1,$VCPU2,$VCPU3,$VCPU4,$VCPU5" >> /etc/tuned/cpu-partitioning-variables.conf
 		tuned-adm profile cpu-partitioning
