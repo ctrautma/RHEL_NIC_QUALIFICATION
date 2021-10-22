82bc0047265fc93cddd8b0ba962ccd2480018d2e
Zhiqiang Fang <zfang@redhat.com>
2021-10-21 23:09:27 -0400

    Update tuned_hugepage_conf.sh

--- throughput-test/tuned_hugepage_conf.sh
+++ throughput-test/tuned_hugepage_conf.sh
@@ -8,4 +8,5 @@ grubby --args='intel_iommu=on iommu=pt default_hugepagesz=1G hugepagesz=1G hugep
 echo "isolated_cores=$PMD_CPU_1,$PMD_CPU_2,$PMD_CPU_3,$PMD_CPU_4,$VCPU1,$VCPU2,$VCPU3,$VCPU4,$VCPU5" > /etc/tuned/cpu-partitioning-variables.conf
 tuned-adm profile cpu-partitioning
 systemctl enable tuned
+efibootmgr -v &>/dev/null && efibootmgr -n $(efibootmgr -v | grep BootCurrent | sed 's/.*: //')
 
